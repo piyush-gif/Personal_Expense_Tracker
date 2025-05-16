8b175ae357a5fff7ec27029a2793215db8b192fb
piyush <piyush.khadka2233@gmail.com>
2025-05-16 22:06:23 +0100

    onnected to the database and created a table

---
+++ database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def connect_db():
+    """Connect to the SQLite database (or create it if it doesn't exist)."""
+    conn = sqlite3.connect("expenses.db")  # Creates 'expenses.db' file if not present
+    return conn
+
+def create_expenses_table():
+  conn = connect_db()
+  cursor = conn.cursor()
+
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS expenses (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date TEXT NOT NULL,
+      amount REAL NOT NULL,
+      category TEXT NOT NULL,
+      description TEXT
+      )
+      """)
+  
+  conn.commit()
+  conn.close()
---
+++ main.py
@@ -0,0 +1,4 @@
+from database import create_expenses_table
+
+create_expenses_table()
+print("Expenses table created (or already exists).")
